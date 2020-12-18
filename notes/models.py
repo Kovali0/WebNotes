994e96f8b557c664783d2206c31281387493b7b4
Kovali0 <kowal.bartek.10@gmail.com>
2020-12-18 23:56:12 +0100

    [Fix][Access controll] Fix the problems with connection between notes and authors. Add access controll for editing and deleting note views.

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
 
 class Note(models.Model):
     title = models.CharField(max_length=256)
     body = models.TextField()
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+    author = models.ForeignKey( User, on_delete=models.CASCADE ) #default = User.objects.get(id=1).pk
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render, redirect
@@ -7,27 +9,55 @@ from django.urls import reverse
 from notes.forms import CustomUserCreationForm
 from notes.models import Note
 
-class NoteListView(ListView):
+class NoteListView(LoginRequiredMixin, ListView):
     model = Note
+
+    def get_queryset(self):
+        return Note.objects.filter(
+            author=self.request.user
+        )#.order_by('-date_posted') #TODO add field of created_date to note model and use it in ordering notes
 
 class NoteDetailView(DetailView):
     model = Note
 
-class NoteCreate(CreateView):
+class NoteCreate(LoginRequiredMixin, CreateView):
     model = Note
     fields = ['title', 'body']
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
+        form.instance.author = self.request.user
         return super(NoteCreate, self).form_valid(form)
 
-class NoteUpdate(UpdateView):
+    def get_success_url(self):
+        return reverse('note-list')
+
+class NoteUpdate(LoginRequiredMixin, UpdateView):
     model = Note
     fields = ['title', 'body']
 
-class NoteDelete(DeleteView):
+    def get_success_url(self):
+        return reverse('note-list')
+
+    def dispatch(self, request, *args, **kwargs):
+        handler = super().dispatch(request, *args, **kwargs)
+        current_note = self.get_object()
+        if not (current_note.author == request.user):
+            raise PermissionDenied
+        return handler
+
+class NoteDelete(LoginRequiredMixin, DeleteView):
     model = Note
     success_url = reverse_lazy('note-list')
+
+    def get_success_url(self):
+        return reverse('note-list')
+
+    def dispatch(self, request, *args, **kwargs):
+        handler = super().dispatch(request, *args, **kwargs)
+        current_note = self.get_object()
+        if not (current_note.author == request.user or request.user.is_superuser):
+            raise PermissionDenied
+        return handler
 
 def Dashboard(request):
     return render(request, "users/dashboard.html")
